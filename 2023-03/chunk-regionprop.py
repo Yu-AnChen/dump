38f2deec3537c440d5fdcf82dbe0ea30b5894b12
Yu-An Chen <atwood12@gmail.com>
2024-09-06 02:09:51 -0700

    Update chunk-regionprop.py

--- 2023-03/chunk-regionprop.py
+++ 2023-03/chunk-regionprop.py
@@ -4,7 +4,9 @@ import numpy as np
 import skimage.measure
 import skimage.segmentation
 import tifffile
+import tqdm
 import zarr
+
 
 _all_mask_props = [
     "label",
@@ -38,7 +40,7 @@ def edger_bbox_to_edge(mask):
     cleared = skimage.segmentation.clear_border(mask)
     mask[cleared > 0] = 0
     if np.all(mask == 0):
-        return np.array(0)
+        return np.array([0])
     # return mask
     results = skimage.measure.regionprops_table(mask)
     rs = results
@@ -81,13 +83,16 @@ def wrap_edger(rrs, rre, ccs, cce):
 
 
 ## %%time
-edge_size = joblib.Parallel(verbose=1, backend="loky", n_jobs=4, return_as="list")(
+edge_size_gen = joblib.Parallel(
+    verbose=0, backend="loky", n_jobs=4, return_as="generator"
+)(
     joblib.delayed(wrap_edger)(rrs, rre, ccs, cce)
     for rrs, rre, ccs, cce in zip(*np.array([rs, re, cs, ce]))
 )
+edge_size = [rr for rr in tqdm.tqdm(edge_size_gen, total=nr * nc)]
+
 es = np.max([np.max(e) for e in edge_size])
 es = 16 * np.ceil(es / 16).astype("int")
-
 
 # ---------------------------------------------------------------------------- #
 #                      joblib for process parallelization                      #
@@ -96,7 +101,7 @@ rechunked = mask.rechunk(1024 * 4)
 rr, cc = np.indices(rechunked.numblocks)
 rsize, csize = rechunked.chunksize
 
-es = 128
+# es = 128
 rs = np.clip(rr * rsize - es, 0, mask.shape[0])
 re = np.clip((rr + 1) * rsize + es, 0, mask.shape[0])
 cs = np.clip(cc * csize - es, 0, None)
@@ -113,10 +118,16 @@ def wrap(rrs, rre, ccs, cce):
 
 
 ## %%time
-block_results = joblib.Parallel(verbose=1, backend="loky", n_jobs=4, return_as="list")(
+_block_results = joblib.Parallel(
+    verbose=0, backend="loky", n_jobs=4, return_as="generator"
+)(
     joblib.delayed(wrap)(rrs, rre, ccs, cce)
     for rrs, rre, ccs, cce in zip(*np.array([rs, re, cs, ce]).reshape(4, -1))
 )
+block_results = [
+    rr for rr in tqdm.tqdm(_block_results, total=np.multiply(*rechunked.numblocks))
+]
+
 
 import pandas as pd
 
@@ -150,7 +161,7 @@ def wrap_int(rrs, rre, ccs, cce, channel=0):
     result = skimage.measure.regionprops_table(
         mm,
         intensity_image=img,
-        properties=("label", "intensity_mean"),
+        properties=("label", "area", "intensity_mean"),
         extra_properties=(quartiles,),
     )
     return result
@@ -177,7 +188,7 @@ block_results_int = joblib.Parallel(
 
 df_block_int = (
     pd.concat([pd.DataFrame(r) for r in block_results_int])
-    .sort_values(["label"])
+    .sort_values(["label", "area"])
     .drop_duplicates(keep="last", subset="label")
     .set_index("label")
 )
